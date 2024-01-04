cb9bd25c3acace03e7de117405e931a8cbcebee3
Mariano Carro <73801345+Mariannkr@users.noreply.github.com>
2024-01-04 19:30:26 -0300

    que onda rey, probando cosas

--- appFortnite/app-fortnite/src/backend/index.jsx
+++ appFortnite/app-fortnite/src/backend/index.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Navbar } from './navbar';  // Asegúrate de tener la ruta correcta
+import { Componente } from './componente';  
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
       <Navbar />
+      <Componente />
   </React.StrictMode>,
+
 )
+
--- appFortnite/app-fortnite/src/backend/navbar.jsx
+++ appFortnite/app-fortnite/src/backend/navbar.jsx
@@ -2,9 +2,20 @@ import React from 'react';
 
 export const Navbar = () => {
   return (
-    <nav className="bg-blue-500 p-4">
-      <div className="container mx-auto" style={{backgroundColor: 'black'}}>
-        <h1 className="text-white text-2xl font-bold">Mi Sitio</h1>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">home</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">TEST</a>
+            </li>
+
+          </ul>
+        </div>
       </div>
     </nav>
   );
