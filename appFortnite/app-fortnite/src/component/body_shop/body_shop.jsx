23c6ee8d42f57df0cb7232a42556274c805c3129
Ignacio Angelone <113468362+IgnacioAngelone@users.noreply.github.com>
2024-01-09 20:38:41 -0300

    Cuerpo #1

---
+++ appFortnite/app-fortnite/src/component/body_shop/body_shop.jsx
@@ -0,0 +1,21 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import './body_shop.css';
+
+{'Comprimir el texto alt + z'}
+
+const body_shop = () => {
+
+     return(
+
+         <body className="body_shop">
+         <h1>Hola</h1>
+         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus nihil quam impedit recusandae adipisci, odio deserunt error voluptatem, ex ipsum facilis dolor cupiditate quod iusto cum nostrum minima dignissimos sapiente.</p>
+         </body>
+
+
+     );
+
+};
+
+export default body_shop;
--- appFortnite/app-fortnite/src/template/shop.jsx
+++ appFortnite/app-fortnite/src/template/shop.jsx
@@ -1,11 +1,13 @@
 /* eslint-disable no-unused-vars */
 import Navbar from '../component/navbar/navbar';
+import Body_shop from '../component/body_shop/body_shop';
 import React from 'react';
 
 const Shop = () => {
   return (
     <div>
       <Navbar />
+      <Body_shop />
     </div>
   );
 };
